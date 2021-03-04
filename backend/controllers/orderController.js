17fbec42acca077743758a22c63ad702f50529f6
gustavo <gustavocruzb@outlook.com>
2021-03-04 16:01:16 -0600

    feature: order by logged in user functionality added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -80,3 +80,12 @@ export const updateOrderToPaid = asyncHandler(async (req, res) => {
 		throw new Error('Order not found');
 	}
 });
+
+//@desc  Get logged in user orders
+//@route GET /api/orders/myorders
+//@acces Private
+
+export const getMyOrders = asyncHandler(async (req, res) => {
+	const orders = await Order.find({ user: req.user._id });
+	res.json(orders);
+});
