aa214c9a535b780fa92db8a568851d4394d51305
gustavo <gustavocruzb@outlook.com>
2021-03-05 12:10:11 -0600

    feature: bakckend admin middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -104,3 +104,11 @@ export const updateUserProfile = asyncHandler(async (req, res) => {
 		throw new Error('User not found');
 	}
 });
+
+//@desc Get all users
+//@route GET /api/users
+//@acces Private/admin
+export const getUsers = asyncHandler(async (req, res) => {
+	const users = await User.find({});
+	res.json(users);
+});
