3728caeb3eed2fabad0133d5f867d3b5608a78f3
gustavo <gustavocruzb@outlook.com>
2021-03-08 18:58:50 -0600

    feature: create and update product endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,3 +38,56 @@ export const deleteProduct = asyncHandler(async (req, res) => {
 		throw new Error('Product not found');
 	}
 });
+
+//@desc  Create a product
+//@route POST /api/products
+//@acces Privates/Admin
+export const createProduct = asyncHandler(async (req, res) => {
+	const product = new Product({
+		name: 'Sample name',
+		proce: 0,
+		user: req.user._id,
+		image: '/images/sample.jpg',
+		brand: 'Sample Brand',
+		category: 'Sample Category',
+		countInStock: 0,
+		numReviews: 0,
+		description: 'SampleDescription',
+	});
+
+	const createdProduct = await product.save();
+	res.status(200).json({ createdProduct });
+});
+
+//@desc  Update a product
+//@route POST /api/products/:id
+//@acces Privates/Admin
+export const updateProduct = asyncHandler(async (req, res) => {
+	const {
+		name,
+		price,
+		description,
+		image,
+		brand,
+		category,
+		countInStock,
+	} = req.body;
+
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		product.name = name;
+		product.price = price;
+		product.description = description;
+		product.image = image;
+		product.brand = brand;
+		product.category = category;
+		product.countInStock = countInStock;
+
+		const updatedProduct = await product.save();
+		res.status(200).json({ updatedProduct });
+	} else {
+		res.status(404);
+		throw new Error('Product not found');
+	}
+});
