c1a3013efc737e3480384b646654058cfd38092c
gustavo <gustavocruzb@outlook.com>
2021-03-09 19:34:42 -0600

    feature: order list admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -89,3 +89,12 @@ export const getMyOrders = asyncHandler(async (req, res) => {
 	const orders = await Order.find({ user: req.user._id });
 	res.json(orders);
 });
+
+//@desc  Get all orders
+//@route GET /api/orders/
+//@acces Private/Admin
+
+export const getOrders = asyncHandler(async (req, res) => {
+	const orders = await Order.find({}).populate('user', 'id name');
+	res.json(orders);
+});
