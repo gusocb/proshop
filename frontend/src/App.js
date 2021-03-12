6fa3cd69824a0afe89dfdf6a5529f7d266ec5b2a
gustavo <gustavocruzb@outlook.com>
2021-03-12 11:50:25 -0600

    feature: searchbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,6 +51,7 @@ const App = () => {
 						path="/admin/orderlist"
 						component={OrderListScreen}
 					/>
+					<Route path="/search/:keyword" component={HomeScreen} />
 					<Route exact path="/" component={HomeScreen} />
 				</Container>
 			</main>
